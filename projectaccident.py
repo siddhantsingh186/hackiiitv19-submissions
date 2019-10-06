1bf40ce7b4aedea9c76d9bb1944437d4bbf10bac
Siddhant <siddhantsingh186@gmail.com>
2019-10-06 10:49:54 +0530

    team#17

--- projectaccident.py
+++ projectaccident.py
@@ -8,12 +8,12 @@ import time
 #the function for checking overspeeding
 def timeandspeed():
 	speed=random.randint()
-	max_speed=120	
+	max_speed = 120
 	if speed>max_speed:
-		p=1	
+		p = 1
 	else:
-		continue
-	if p==1:
+		p = 0
+	if p == 1:
 		print("Light on")
 	return p
 
@@ -21,18 +21,18 @@ def timeandspeed():
 #the function for monitoring axel rotation
 
 def position():
-	angle=float(input("Value from accelerometer")) #input from accelerometer
-	c=0    	
+	angle = float(input("Value from accelerometer")) #input from accelerometer
+	c = 0
 	if angle<0:
 		angle= -(angle)
-	if angle > 33.7:
-		c=1
+	if angle > 27:
+		c = 1
 	return c
 
 #the function for calculation of acceleration at time of crash
 def accident():
 	accl=input()
-	fix_accl=80.0
+	fix_accl = 20.0
 	if accl<0:
 		accl=-accl
 	if accl>fix_accl:
@@ -42,42 +42,43 @@ def accident():
 
 def acceleration():
 	accl = int(input())
-	return accl 
+	return accl
 
 #snippet to check the violation of rules and accordingly upload the data to the cloud based server
 accl_previous = acceleration()
 counter_speed = 1;
 counter_axel_rotation = 1
-while True: 
+counter_acceleration_var = 1
+while True:
 	accl_present = acceleration()
 	pos = position()
 	if pos == 1:
 		counter_axel_rotation+=1
 		print("Beep On")
 	if counter_axel_rotation>=5:
-		s="beep on"*2
-		print(s)
+		s="beep on"
+		print(s + s)
 		counter_axel_rotation=0
 		#pass on information to cloud based server
 	acc=acceleration()
-	if (accl_present-accl_previous) >= 15:
+	if (accl_present-accl_previous) >= 3:
                 print("Beep on")
-		#pass on information to cloud based server 	    
-	speed_check=timeandspeed()        
+		counter_acceleration_var += 1
+	if counter_acceleration_var >= 5
+		counter_acceleration_var = 0
+		#pass on information to cloud based server
+	speed_check = timeandspeed()
 	if speed_check:
-           	counter_speed+=1
+           	counter_speed += 1
 	if counter_speed >= 5:
                 counter = 0
               #pass on information to cloud based server
 	accl_previous = accl_present;
-	acci=accident()
+	acci = accident()
 	if acci == 1:
 		print("Light ON")
 		#pass on information to cloud based server
-	time.sleep(0.5)         
-
-
-
-
-
-
+		counter_acceleration_var + =  1
+	if counter_acceleration_var >= 5
+		#pass on information to cloud based server
+	time.sleep(1.5)
