3fa82b9436d68732cb778e1bc78f6bc87f0c2cdb
Siddhant <siddhantsingh186@gmail.com>
2019-10-06 09:17:17 +0530

    team#17

--- projectaccident.py
+++ projectaccident.py
@@ -18,16 +18,15 @@ def timeandspeed():
 	return p
 
 
-#the function for checking overspeeding
-def timeandspeed():
-	speed=random.randint()
-	max_speed=120	
-	if speed>max_speed:
-		p=1	
-	else:
-		continue
-	if p==1:
-		print("Light on")
-	return
+#the function for monitoring axel rotation
+
+def position():
+	angle=float(input("Value from accelerometer")) #input from accelerometer
+	c=0    	
+	if angle<0:
+		angle= -(angle)
+	if angle > 33.7:
+		c=1
+	return c
 
 
